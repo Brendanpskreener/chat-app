4594926ffad6bc2451268eba5e60681f23329bb7
Brendan Schreiner <brendanpschreiner@gmail.com>
2024-04-30 14:02:52 -0700

    basic setup

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,4 +1,5 @@
 const socket = io()
+
 socket.on('message', (message) => {
   console.log(message)
 })
@@ -7,4 +8,16 @@ document.querySelector('#message-form').addEventListener('submit', (event) => {
   event.preventDefault()
   const message = event.target.elements.message.value
   socket.emit('sendMessage', message)
+})
+
+document.querySelector('#send-location').addEventListener('click', () => {
+  if (!navigator.geolocation) {
+    return alert('location unavailable')
+  }
+  navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
+    socket.emit('sendLocation', {
+      latitude,
+      longitude
+    })
+  })
 })
